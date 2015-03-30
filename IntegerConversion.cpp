d3d852924c986caa0e6849723db046aef3932a98
21stdragonhunter <colemansmith21@gmail.com>
2015-03-30 12:21:53 -0400

    Update IntegerConversion.cpp

--- IntegerConversion.cpp
+++ IntegerConversion.cpp
@@ -2,4 +2,24 @@
 #include "vector"
 #include "string"
 
-vector<int> 
+vector<int> toBinary(int decimal) {
+  
+  vector<int> returnVector;
+  vector<int> reverseVector;
+  
+  while(decimal > 0) {
+    
+    reversedVector.append(decimal % 2);
+    decimal /= 2;
+    
+  }
+  
+  for(int i = reversedVector.length; i > 0; i--) {
+    
+    returnVector.append(reversedVector[i-1]);
+    
+  }
+  
+  return returnVector;
+  
+}
